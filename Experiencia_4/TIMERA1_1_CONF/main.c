df7755bc80fcd78b924d0c7d6e8a474af23953bf
Fokappa <fffonseca@uc.cl>
2019-06-09 05:40:32 -0400

    Exp4 creo que terminado 1.1

--- Experiencia_4/TIMERA1_1_CONF/main.c
+++ Experiencia_4/TIMERA1_1_CONF/main.c
@@ -5,19 +5,19 @@
 #include <math.h>
 #include "USART/USART_implement_me.h"
 
-#define BIENVENIDA "Bienvenido al control de Temperatura \r\nEn cualquier instante envie las siguientes cifras para cambiar: 1: Set Point, 2: Kp, 3: Ki \r\n"
+#define BIENVENIDA "Bienvenido al control de Temperatura \r\nEn cualquier instante envie las siguientes cifras para cambiar: \r\n1: Set Point, 2: Kp, 3: Ki \r\n"
 #define RECORDAR    "1: Set Point, 2: Kp, 3: Ki \r\n"
 /*********************************************/
 /************VARIABLES GLOBALES***************/
 int lista[10];
-int l;
-int j;
-int k;
+uint8_t l;
+uint8_t j;
+uint8_t k;
 long double k_p = 0;
 long double k_i = 0;
 float integral = 0;
 float duty = 0;
-int pos = 0;
+uint8_t pos = 0;
 double set = 0;
 float voltaje = 0;
 uint8_t ESTADO_WHILE = 0;
@@ -29,6 +29,7 @@ char choice;
 void IO_CONFIGURATION(void);
 void TIMER_CONFIGURATION(void);
 void PID_INSERT(void);
+void ADC_CONFIGURATION(void);
 /*************************/
 
 void main(void)
@@ -41,20 +42,25 @@ void main(void)
 
 	USART_Init(); //CONFIGURANDO USART 9600BAUD 8DATOS 1STOP NOPAR
 
+	ADC_CONFIGURATION(); //CONFIGURANDO ADC
+
     USART_Transmit_String(BIENVENIDA);
 
     __enable_interrupt();
-    int wea;
+    uint8_t wea;
     while(1){
         if(ESTADO_WHILE){
             PID_INSERT();
             ESTADO_WHILE = 0;
-            for(wea = 0; wea<5;wea++) USART_Transmit_String("\n");
+            for(wea = 0; wea<3;wea++) USART_Transmit_String("\n");
             USART_Transmit_String(RECORDAR);
         }
+        _delay_cycles(16000);
+        ADC10CTL0 |=  ADC10IFG;
     }
 }
-
+/**********************************************************************************/
+/*pragma vectores*/
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void ISR_PARA_UART_RX(void)
 {
@@ -66,7 +72,6 @@ __interrupt void ISR_PARA_UART_RX(void)
 
 #pragma vector = TIMER1_A1_VECTOR
  __interrupt void TIMER1_A1_ISR(void){
-    P1OUT  ^=   BIT0;
     TA1CCR1 = duty;  //duty cycle
     float error = 0;
     double value = 0;
@@ -92,8 +97,29 @@ __interrupt void ISR_PARA_UART_RX(void)
     // duty = (int) escalado;
     duty = escalado;
     TA1CTL &= ~TAIFG;
-
  }
+
+#pragma vector = ADC10_VECTOR
+__interrupt void adc10_interrupt(void)
+{
+    P1OUT ^= BIT0; //LED QUE INDICA VELOCIDAD DE ADC
+    ADC10CTL0 |= ENC + ADC10SC; // Start Conversion again
+    while(ADC10CTL1 & ADC10BUSY);
+    voltaje = ADC10MEM*3.3/1023;
+    lista[pos] = voltaje; //
+    if (pos == 10){
+        pos = 0;
+    }
+    else{
+        pos++;
+    }
+    ADC10CTL0 &=~ ADC10IFG;
+}
+ /**********************************************************************************/
+
+
+
+
 /********************************************************************************************************/
 /*************CONFIGURATION********************/
 void IO_CONFIGURATION(void){
@@ -103,6 +129,9 @@ void IO_CONFIGURATION(void){
     //TIMER
     P2DIR  |= BIT0 | BIT1;
     P2SEL  |= BIT0 | BIT1;
+    //ADC
+    P1DIR &= ~BIT5;
+    P1SEL |=  BIT5; //P1.5 Funcion ADC
     //LED 1.0
     P1DIR |= BIT0;
     P1OUT &=~ BIT0;
@@ -114,6 +143,14 @@ void TIMER_CONFIGURATION(void){
     TA1CCR0 = 655;// Máximo del counter, 5ms de resolución (frecuencia PWM 200Hz)
     TA1CCR1 = 200;  //duty cycle
     TA1CTL |= TASSEL_2 | MC_1 | ID_3 | TAIE & ~TAIFG;
+}
+
+void ADC_CONFIGURATION(void){
+    ADC10CTL0 |= ADC10ON & ~ENC;
+    ADC10CTL0 |= SREF_0 + ADC10SHT_0;                              // Vcc reference, 16 clocks, on
+    ADC10CTL1 |= INCH_5 + ADC10DIV_0 + ADC10SSEL_2 + CONSEQ_0;
+    ADC10AE0  |= BIT5;                                                        // Entrada del ADC en P1.5
+    ADC10CTL0 |= ADC10IE;                                                   // Se enciende y se habilitan interrupciones
 }
 /********************************************************************************************************/
 
